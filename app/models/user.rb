157c5398574be169feb6b0f83cda6e62894fc1ff
James O'Neal <jamesponeal@gmail.com>
2017-01-10 19:34:16 -0800

    update to migration and model files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :shelves, foreign_key: :owner_id
+  has_many :reviews, foreign_key: :author_id
+  has_many :ratings, foreign_key: :rater_id
+
+  has_attached_file :image, :styles => {:small => "200x200", :medium => "400x400", :large => "600x600"}
+
+  validates_attachment :image, :content_type => {:content_type => /^image\/(jpeg|png|gif|tiff)$/ }
+
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, :last_name, :admin, presence: true
+
 end
--- db/migrate/20170108195319_create_ratings.rb
+++ db/migrate/20170108195319_create_ratings.rb
@@ -1,6 +1,9 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
+      t.integer :rating
+      t.integer :rater_id
+      t.integer :movie_id
 
       t.timestamps null: false
     end
