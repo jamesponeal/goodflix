c7a22c995220ce7bbd27dccc940dd073f54e6b50
James O'Neal <jamesponeal@gmail.com>
2017-01-12 22:21:44 -0800

    corrections and additions to migration and model files

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ActiveRecord::Base
   has_many :reviews, foreign_key: :author_id
   has_many :ratings, foreign_key: :rater_id
 
-  has_attached_file :image, :styles => {:small => "200x200", :medium => "400x400", :large => "600x600"}
-
-  validates_attachment :image, :content_type => {:content_type => /^image\/(jpeg|png|gif|tiff)$/ }
-
   validates :username, presence: true, uniqueness: true
   validates :first_name, :last_name, :admin, presence: true
 
