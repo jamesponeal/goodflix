2a12a49ace022e0f14c885105ac676fbef38838c
James O'Neal <jamesponeal@gmail.com>
2017-01-15 13:55:42 -0800

    adds user show page and login button

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,13 @@
 module UserHelper
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
   resources :users
+
+  get '/logout' => 'users#destroy', as: 'logout'
+  post '/login' => 'users#login', as: 'login'
 
 
 end
