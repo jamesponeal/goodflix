fc50559464c29e9e8ba7ce9a599a9b4fd01df81c
James O'Neal <jamesponeal@gmail.com>
2017-01-15 11:53:49 -0800

    adds paperclip migration file

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   has_many :reviews, foreign_key: :author_id
   has_many :ratings, foreign_key: :rater_id
 
+  has_attached_file :image, :styles => {:small => "200x200", :medium => "500x500", :large => "800x800"}
+
+  validates_attachment :image, :content_type => {:content_type => /^image\/(jpeg|png|gif|tiff)$/ }
+
   validates :username, presence: true, uniqueness: true
   validates :first_name, :last_name, :admin, presence: true
 
