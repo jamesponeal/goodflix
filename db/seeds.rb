7e42e66d36d549236feac6729e12da141c2e2eb3
James O'Neal <jamesponeal@gmail.com>
2017-01-17 18:38:15 -0800

    added shelves list to user show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create!(first_name: "Jim", last_name: "O'Neal", username: "jamesponeal", password: "starwars", admin: true, bio: "Here I am!")
+
+Shelf.create!(owner_id: 1, name: "Watched")
+Shelf.create!(owner_id: 1, name: "Want To Watch")
+
+Movie.create!(title:"Star Wars", duration: 121, summary:"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.", rating: "PG")
