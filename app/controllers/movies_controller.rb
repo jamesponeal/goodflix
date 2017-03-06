98c15f9e978bb80138e7f947cfb3643daf2e4790
James O'Neal <jamesponeal@gmail.com>
2017-03-05 18:08:46 -0800

    adds movie show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,13 @@
 class MoviesController < ApplicationController
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    if @movie
+      render "show"
+    else
+      flash[:notice] = "Sorry, this movie was not found!"
+      redirect_to :back
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :users
-
   resources :shelves
+  resources :movies
 
   get '/logout' => 'users#destroy', as: 'logout'
   post '/login' => 'users#login', as: 'login'
